68f48dfddf82aef21bbd56485170ccdb452c0f6a
daria_faskhieva <dashaf712@mail.ru>
2023-04-10 17:03:24 +0300

    Сделан TabControl

---
+++ Technical_Software_Service/Windows/Window_Users.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Technical_Software_Service
+{
+    /// <summary>
+    /// Логика взаимодействия для Window_Users.xaml
+    /// </summary>
+    public partial class Window_Users : Window
+    {
+        public Window_Users()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
